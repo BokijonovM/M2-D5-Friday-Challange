e1e1414d62118f108dc94231bcec8bb5e3f966b7
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2021-12-10 16:01:52 +0100

    updated but still not working

--- js/back-office.js
+++ js/back-office.js
@@ -1,43 +1,176 @@
-let test
+let movieId
+let category
+let END_POINT
+let method
+window.onload = () => {
+    movieId = new URLSearchParams(window.location.search).get('movie_Id')
+    category = new URLSearchParams(window.location.search).get('category')
+    removeSpinner()
+    END_POINT = movieId ? `https://striveschool-api.herokuapp.com/api/movies/` + movieId : "https://striveschool-api.herokuapp.com/api/movies/"
+    method = movieId ? "PUT" : "POST"
+
+    if (category)
+        getTitleData(category)
+}
 
 
-function getFilms() {
-    let film = {
-        name: document.getElementById('film_name').value,
-        description: document.getElementById('film_category').value,
-        brand: document.getElementById('film_img').value,
-        imageUrl: document.getElementById('film_descr').value
+async function getTitleInformation(e) {
+    e.preventDefault()
+
+    let body = {
+        category: document.querySelector('#film_category').value,
+        name: document.querySelector('#film_name').value,
+        imageUrl: document.querySelector('#film_img').value,
+        description: document.querySelector('#film_descr').value,
     }
-    let filmStringified = JSON.stringify(film)
-    return filmStringified
+
+
+
+    await sendTitleToServer(body)
+}
+
+
+async function sendTitleToServer(titleData) {
+    try {
+        let dataSent = await fetch(END_POINT, {
+            method: method,
+            body: JSON.stringify(titleData),
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzcyNTksImV4cCI6MTY0MDM0Njg1OX0.dxQCJ4P9B9hsCfAVADv3O3EEMc3fVV7XO3JdcwbB0eI",
+                "Content-Type": "application/json",
+            },
+        })
+        if (dataSent.ok != true)
+            throw err
+        let dataReceived = await dataSent.json()
+
+        document.querySelector('form').reset()
+        displayAlert('success', `${titleData.name} added successfully`)
+    } catch (err) {
+        displayAlert('danger', `${err}`)
+    }
 
 }
 
 
-
-async function insertFilm(e) {
+async function deleteTitle(e) {
+    e.preventDefault()
     try {
 
-        e.preventDefault()
-        let film = getFilms()
-        let serverRes = await fetch("https://striveschool-api.herokuapp.com/api/movies", {
-            method: "POST",
-            body: film,
+        let dataDeleted = await fetch(END_POINT, {
+            method: "DELETE",
             headers: {
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2Mzg5NzI4NjUsImV4cCI6MTY0MDE4MjQ2NX0.nLnmXj2cSk_s7mU43hZR2pjgHFHPydHPZLzMi8WQfsM",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzcyNTksImV4cCI6MTY0MDM0Njg1OX0.dxQCJ4P9B9hsCfAVADv3O3EEMc3fVV7XO3JdcwbB0eI",
                 "Content-Type": "application/json",
-            }
+            },
         })
+        if (dataDeleted.ok != true)
+            throw err
+        displayAlert('success', `Title deleted successfully`)
 
-        let serverData = await serverRes.json()
-        console.log(serverData)
-        document.querySelector('form').reset()
-        alert(`${serverData.name} added successfully`)
-    } catch (e) {
-        alert(e)
+        setInterval(() => location.replace("file:///C:/Users/drdve/OneDrive/Homework__StriveSchool/Module03/M3-assestmentNetflix/M3-Assestment/M2-D5/index.html"), 3000)
+
+    } catch (err) {
+        displayAlert('danger', `${err}`)
     }
 
 }
+
+
+async function getTitleData(category) {
+    console.log(category)
+    try {
+
+        let response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/${category}/`, {
+            headers: {
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzcyNTksImV4cCI6MTY0MDM0Njg1OX0.dxQCJ4P9B9hsCfAVADv3O3EEMc3fVV7XO3JdcwbB0eI",
+                "Content-Type": "application/json",
+            },
+
+        })
+        if (response.ok != true)
+            throw err
+        let moviesData = await response.json()
+        let movie = moviesData.filter(movie => movie._id === movieId)
+
+        let inputs = document.querySelectorAll('.form-control')
+        inputs.forEach(input => {
+            input.value = movie[0][`${input.id}`]
+        })
+
+        document.getElementById('sendBtn').innerText = 'Update movie'
+        throw err
+    } catch (err) {
+
+    }
+
+}
+
+
+
+
+
+
+// Aux funcs
+function displayAlert(status, msg) {
+    let htmlRef = document.querySelector('nav')
+    htmlRef.insertAdjacentHTML('afterend', `
+                                                    <div class="alert alert-${status}" role="alert">
+                                                        ${msg}
+                                                    </div>`
+    )
+    setTimeout(() =>
+        document.querySelector('.alert').remove(), 3000)
+}
+
+function removeSpinner() {
+    document.querySelector('.loading').remove()
+}
+
+
+
+
+
+
+
+
+// function getMovies() {
+//     let movie = {
+//         name: document.getElementById('film_name').value,
+//         category: document.getElementById('film_category').value,
+//         imageUrl: document.getElementById('film_img').value,
+//         description: document.getElementById('film_descr').value
+//     }
+//     let filmStringified = JSON.stringify(movie)
+//     return filmStringified
+
+// }
+
+
+
+// async function insertMovie(e) {
+//     try {
+
+//         e.preventDefault()
+//         let movie = getMovies()
+//         let serverRes = await fetch("https://striveschool-api.herokuapp.com/api/movies", {
+//             method: "POST",
+//             body: movie,
+//             headers: {
+//                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzk4OTIsImV4cCI6MTY0MDM0OTQ5Mn0.DT_Ay4de_IDGvXgWg1I1ksPDwlbYY_opUvt8rkgSYaU",
+//                 "Content-Type": "application/json",
+//             }
+//         })
+
+//         let serverData = await serverRes.json()
+//         console.log(serverData)
+//         document.querySelector('form').reset()
+//         alert(`${serverData.name} added successfully`)
+//     } catch (e) {
+//         alert(e)
+//     }
+
+// }
 
 // let userId = new URLSearchParams(window.location.search).get('userId')
 // const url = userId ? "https://striveschool-api.herokuapp.com/api/product/" + userId : "https://striveschool-api.herokuapp.com/api/product/"
--- js/script.js
+++ js/script.js
@@ -1,12 +1,182 @@
-const hideAppear = function(){
+const hideAppear = function () {
     let iconId = document.getElementById("search-icon-nav")
     let searchInput = document.getElementById("nav-form-control")
     iconId.style.display = "none"
     searchInput.style.display = "block"
 }
-const hideAppear1 = function(){
+const hideAppear1 = function () {
     let iconId = document.getElementById("search-icon-nav")
     let searchInput = document.getElementById("nav-form-control")
     iconId.style.display = "block"
     searchInput.style.display = "none"
 }
+
+
+// Fetching
+
+async function getCategories() {
+    let response = await fetch('https://striveschool-api.herokuapp.com/api/movies/', {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzk4OTIsImV4cCI6MTY0MDM0OTQ5Mn0.DT_Ay4de_IDGvXgWg1I1ksPDwlbYY_opUvt8rkgSYaU",
+            "Content-Type": "application/json",
+        },
+    })
+    let categories = await response.json()
+    categories.pop()
+    createSections(categories)
+    loadTitles(categories)
+
+}
+
+
+function createSections(categories) {
+    let categoriesDeck = document.querySelector('.movie-categories-html')
+
+    categories.forEach(category => {
+        let newSection = document.createElement('a')
+        newSection.classList.add('row', 'dropdown-item', 'm-0', 'pb-0')
+
+        let sectionTitle = document.createElement('row')
+        sectionTitle.classList.add('m-0')
+        sectionTitle.innerText = category
+        newSection.appendChild(sectionTitle)
+
+        let productDeck = document.createElement('div')
+        productDeck.id = category
+        newSection.insertAdjacentElement('beforeend', productDeck)
+
+        categoriesDeck.insertAdjacentElement('afterbegin', newSection)
+    });
+
+}
+
+window.onload = () => {
+    getCategories()
+
+}
+
+async function loadTitles(categories) {
+
+    categories.forEach(async function (category) {
+        let titles = await getTitles(category)
+
+        titles.forEach(title => {
+
+            let categorySection = document.querySelector(`#${title.category}`)
+            categorySection.insertAdjacentHTML("afterbegin", `${generateTitlesHTML(title)}`)
+
+        })
+
+    })
+
+}
+
+async function getTitles(category) {
+    let response = await fetch(`https://striveschool-api.herokuapp.com/api/movies/${category}`, {
+        headers: {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzk4OTIsImV4cCI6MTY0MDM0OTQ5Mn0.DT_Ay4de_IDGvXgWg1I1ksPDwlbYY_opUvt8rkgSYaU",
+            "Content-Type": "application/json",
+        },
+    })
+    let titles = await response.json()
+    return titles
+}
+
+
+function generateTitlesHTML(title) {
+    return `
+        <div class="card d-none col align-items-center mb-4 py-4 px-1">
+                <img src="${title.imageUrl}" class="card-img-top img-fluid" alt="...">       
+            <a href="./backoffice.html?movie_Id=${title._id}&category=${title.category}" class="mt-3"> Edit</a>
+        </div>
+    `
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// async function getMovies() {
+//     try {
+//         let movieJSON = await fetch("https://striveschool-api.herokuapp.com/api/movies", {
+//             headers: {
+//                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYjA3YTRjZmY1ZjAwMTU5MGJkYjMiLCJpYXQiOjE2MzkxMzk4OTIsImV4cCI6MTY0MDM0OTQ5Mn0.DT_Ay4de_IDGvXgWg1I1ksPDwlbYY_opUvt8rkgSYaU"
+//                 ,
+//             }
+//         })
+//         let movieList = await movieJSON.json()
+//         movieList.shift()
+
+//         let brandList = getNames(movieList)
+//         createSections(brandList)
+//         appendCards(movieList)
+
+//     } catch (e) {
+//         console.log(e)
+//         alert(e)
+//     }
+// }
+
+// window.onload = () => {
+//     getMovies()
+
+// }
+// function createSections(names) {
+//     let hero = document.querySelector('.list-movies')
+//     names.forEach(name => {
+//         let newSection = document.createElement('section')
+//         newSection.classList.add('container', 'px-0')
+
+//         let sectionTitle = document.createElement('h2')
+//         sectionTitle.classList.add('row', 'mx-0')
+//         sectionTitle.innerText = name
+//         newSection.appendChild(sectionTitle)
+
+//         let productDeck = document.createElement('div')
+//         productDeck.classList.add('row', 'productsDeck')
+//         newSection.insertAdjacentElement('beforeend', productDeck)
+
+//         hero.insertAdjacentElement('afterend', newSection)
+//     });
+
+// }
+
+// function getNames(data) {
+//     let names = []
+//     data.filter(movie => {
+//         if (names.includes(movie.name) === false)
+//             names.push(movie.name)
+//     })
+//     return names
+// }
+
+// function generateCards(movie) {
+//     return `
+//             <div class="col-sm-5 col-md-4 col-lg-3 col-xl-2 pt-2 pb-2 pl-0 pr-1 img-hover-zoom">
+//                 <img src="${movie.imageUrl}" class="card-img-top" alt="..." width="100%" height="100%" />
+//                 <h5 class="card-title text-truncate d-none">Name: ${movie.name}</h5>
+//                 <span class="badge badge-warning mx-3 d-none">${movie.category}</span>
+//                 <p class="card-text mb-3" id="description d-none">Description:  ${movie.description}</p>
+//             </div>
+//             `
+// }
+
+
+// function appendCards(movies) {
+//     movies.forEach(movie => {
+
+//         let sectionToInsert = document.querySelector(`#${movie.name.toLowerCase()}`)
+
+//         sectionToInsert.insertAdjacentHTML('beforeend', `${generateCards(movie)}`)
+//     })
+// }
+
+
+
